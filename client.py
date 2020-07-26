496a086f1afc9dd90b31d761f9e0605678d2d229
syntaxnoob <syntaxnoob@pop-os.localdomain>
2020-07-26 16:26:18 +0200

    'small' update

--- client.py
+++ client.py
@@ -1,9 +1,26 @@
-#!/bin/python3
+#!/bin/python3.8
 
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 2205))
+HEADERSIZE = 10
 
-msg = s.recv(1024)
-print(msg.decode('utf-8'))
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 2206))
+
+while True:
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print("new message lenght = {msg[:HEADERSIZE]}")
+            msglenght = int(msg[:HEADERSIZE])
+            new_msg = False
+        full_msg += msg.decode('utf-8')
+        if len(full_msg)-HEADERSIZE == msglenght:
+            print('the full message is reseaved.')
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ''
+    print(full_msg)
--- server.py
+++ server.py
@@ -1,11 +1,25 @@
-#!/bin/python3
-
+#!/bin/python3.8
+# test
 import socket
+import time
+HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 2205))
+s.bind((socket.gethostname(), 2206))
 s.listen(5)
+
 while True:
     clientsocket, address = s.accept()
-    print(f'Connection from{address} has been established.')
-    clientsocket.send(bytes("Welcome!", "utf-8"))
+    print("Connection from{address} has been established.")
+
+    msg = "Welcome to the server"
+    msg = f"{len(msg):<{HEADERSIZE}}" + msg
+
+    clientsocket.send(bytes(msg, "utf-8"))
+
+    # while True:
+    #     time.sleep(3)
+    #     msg = input("send a message:")
+    #     msg = f"{time.time()+msg}"
+    #     msg = (f'{len(msg):<{HEADERSIZE}}') + msg
+    #     clientsocket.send(bytes(msg, "utf-8"))
